bcec0f1b0f1415fd691275dad02afd9d32927073
Emil Hartmann <emil03hartmann@gmail.com>
2026-02-04 22:00:11 +0100

    put results directly in tar without folder around it

--- grep_emil_results.sh
+++ grep_emil_results.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [[ -z "$1" ]]; then
-  echo "Usage: $0 <prefix>"
-  exit 1
-fi
 
 PREFIX="$1"
 CURRENT_DIR="$(basename "$PWD")"
@@ -10,4 +6,8 @@ TAR_NAME="${CURRENT_DIR}_execution_csvs.tar"
 
 find . -maxdepth 1 -type d -name "${PREFIX}*" \
   -exec find {} -type f -name "*_execution.csv" \; \
-  | tar -cvf "$TAR_NAME" -T -
+  | tar -cvf "$TAR_NAME" \
+        --transform='s|.*/||' \
+        -T -
+
+echo "Archiv erstellt (flach): $TAR_NAME"
