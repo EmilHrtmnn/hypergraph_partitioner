5feebdde2e614f0f2929db4142af5ea3f692098d
Emil Hartmann <emil03hartmann@gmail.com>
2026-02-04 22:15:55 +0100

    rename tar

--- grep_emil_results.sh
+++ grep_emil_results.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
 PREFIX="$1"
 CURRENT_DIR="$(basename "$PWD")"
-TAR_NAME="${CURRENT_DIR}_execution_csvs.tar"
+TAR_NAME="${CURRENT_DIR}.tar"
 
 find . -maxdepth 1 -type d -name "${PREFIX}*" \
   -exec find {} -type f -name "*_execution.csv" \; \
   | tar -cvf "$TAR_NAME" \
         --transform='s|.*/||' \
         -T -
-
-echo "Archiv erstellt (flach): $TAR_NAME"
